253305007ad047a4a7a1df420a1c6bd85d3e8971
Pavan Chaudhari <pavchadrive@gmail.com>
2023-12-22 02:22:01 -0500

    problems 88-90 	new file:   inputs/0089_roman.txt 	modified:   problems/51-100/problem86.ipynb 	modified:   problems/51-100/problem88.ipynb 	new file:   problems/51-100/problem89.ipynb 	new file:   problems/51-100/problem90.ipynb 	new file:   problems/51-100/problem91.ipynb 	new file:   reusable_code/factorize.py 	deleted:    reusable_code/prime_factorize.py

---
+++ reusable_code/factorize.py
@@ -0,0 +1,43 @@
+from sieve_of_eratosthenes import sieve
+from collections import Counter
+
+def prime_factorize(num, prime_bins = None, primes = None):
+    prime_factors = []
+    if not primes:
+        prime_bins, primes = sieve(num)
+    
+    if prime_bins[n]:
+        return Counter([n])
+
+    n = num
+    for p in primes:
+        if n == 1 or p > n:
+            break
+        while n % p == 0:
+            prime_factors.append(p)
+            n //= p
+
+    return Counter(prime_factors)
+
+def factorize(num, prime_bins = None):
+    if not prime_bins:
+        prime_bins, _ = sieve(num)
+
+    if prime_bins(num):
+        return [1,num]
+    
+    left_factors = [1]
+    right_factors = [num]
+    i = 2
+
+    while i*i < num:
+        if num % i:
+            left_factors.append(i)
+            right_factors = [num // i] + right_factors
+
+        i += 1
+
+    if i*i == num:
+        left_factors.append(i)
+
+    return left_factors + right_factors
